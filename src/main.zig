a26678035a6e337d4693032c3a8fb7b863df79ca
Nitin Prakash <prakash.nitin63@gmail.com>
2023-10-21 09:33:51 +0530

    Part 2: Recognize INSERT and SELECT commands on the REPL

--- src/main.zig
+++ src/main.zig
@@ -5,16 +5,73 @@ const io = std.io;
 const process = std.process;
 const expect = std.testing.expect;
 
+const InputBuffer = struct {
+    buffer: [128]u8,
+};
+
+const MetaCommandResult = enum {
+    metaSuccess,
+    metaUnrecognizedCommand,
+};
+
+const PrepareResult = enum {
+    prepareSuccess,
+    // TOOO: this needs to be an exception in the logic
+    prepareUnrecognizedCommand,
+};
+
+const StatementType = enum {
+    stmtSelect,
+    stmtInsert,
+};
+
+const Statement = struct {
+    stype: StatementType,
+};
+
+fn do_meta_command(input_buffer: []const u8) MetaCommandResult {
+    const exit_command = ".exit";
+
+    if (std.mem.eql(u8, input_buffer, exit_command)) {
+        return MetaCommandResult.metaSuccess;
+    }
+    return MetaCommandResult.metaUnrecognizedCommand;
+}
+
 fn read_input() ![]const u8 {
     const stdin = std.io.getStdIn().reader();
 
-    var buf: [1024]u8 = undefined;
+    var buf: [124]u8 = undefined;
 
     // TODO: use `streamUntilDelimiter` instead
     if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |user_input| {
         return user_input;
     } else {
         return error.EndOfStream;
+    }
+}
+
+fn prepare_statement(input_buffer: []const u8, statement: *Statement) PrepareResult {
+    if (std.mem.eql(u8, input_buffer, "insert")) {
+        statement.*.stype = StatementType.stmtInsert;
+        return PrepareResult.prepareSuccess;
+    }
+    if (std.mem.eql(u8, input_buffer, "select")) {
+        statement.*.stype = StatementType.stmtSelect;
+        return PrepareResult.prepareSuccess;
+    }
+
+    return PrepareResult.prepareUnrecognizedCommand;
+}
+
+fn execute_statement(statement: *Statement) void {
+    switch (statement.*.stype) {
+        .stmtSelect => |_| {
+            std.debug.print("This is a SELECT statement\n", .{});
+        },
+        .stmtInsert => |_| {
+            std.debug.print("This is a INSERT statement\n", .{});
+        },
     }
 }
 
@@ -26,20 +83,30 @@ pub fn main() !void {
     while (true) {
         _ = print_prompt();
 
-        const command = try read_input();
-        const exit_command = ".exit";
+        const input_buffer = try read_input();
 
-        if (std.mem.eql(u8, command, exit_command)) {
-            process.exit(0);
-        } else {
-            std.debug.print("Unrecognized command {s}\n", .{command});
+        if (input_buffer[0] == '.') {
+            const result = do_meta_command(input_buffer);
+            switch (result) {
+                .metaSuccess => |_| {
+                    os.exit(0);
+                },
+                .metaUnrecognizedCommand => |_| {
+                    std.debug.print("Unrecognized command\n", .{});
+                    continue;
+                },
+            }
+        }
+
+        var statement: Statement = undefined;
+        const prep_statement = prepare_statement(input_buffer, &statement);
+        switch (prep_statement) {
+            .prepareSuccess => |_| {
+                execute_statement(&statement);
+            },
+            .prepareUnrecognizedCommand => |_| {
+                std.debug.print("Unrecognized keyword: {s}\n", .{input_buffer});
+            },
         }
     }
-}
-
-test "slices" {
-    const msg = try read_input();
-    const input = ".exit";
-    std.debug.print("{s}, {s}\n", .{ msg, input });
-    try std.testing.expect(std.mem.eql(u8, msg, input));
 }
